450dba1bb219931ee436e39b2ce2cfb61d6c7777
fzshuai <156117106@qq.com>
2024-04-20 19:27:24 +0800

    fix 修复登录注册功能

--- src/components/model/LoginModel.vue
+++ src/components/model/LoginModel.vue
@@ -8,8 +8,8 @@
         <!-- 用户名 -->
         <v-text-field
           v-model="username"
-          label="邮箱号"
-          placeholder="请输入您的邮箱号"
+          label="账号"
+          placeholder="请输入您的账号"
           clearable
           @keyup.enter="login"
         />
@@ -38,10 +38,6 @@
         <div class="mt-10 login-tip">
           <span @click="openRegister">立即注册</span>
           <span @click="openForget" class="float-right">忘记密码?</span>
-        </div>
-        <div v-if="socialLoginList.length > 0">
-          <div class="social-login-title">社交账号登录</div>
-          <div class="social-login-wrapper"></div>
         </div>
       </div>
     </v-card>
@@ -72,14 +68,6 @@ export default {
         return false;
       }
       return true;
-    },
-    socialLoginList() {
-      return this.$store.state.blogInfo.websiteConfig.socialLoginList;
-    },
-    showLogin() {
-      return function(type) {
-        return this.socialLoginList.indexOf(type) != -1;
-      };
     }
   },
   methods: {
@@ -92,9 +80,8 @@ export default {
       this.$store.state.forgetFlag = true;
     },
     login() {
-      var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-      if (!reg.test(this.username)) {
-        this.$toast({ type: "error", message: "邮箱格式不正确" });
+      if (this.username.trim().length == 0) {
+        this.$toast({ type: "error", message: "账号不能为空" });
         return false;
       }
       if (this.password.trim().length == 0) {
@@ -102,89 +89,22 @@ export default {
         return false;
       }
       const that = this;
-      // eslint-disable-next-line no-undef
-      var captcha = new TencentCaptcha(this.config.TENCENT_CAPTCHA, function(
-        res
-      ) {
-        if (res.ret === 0) {
-          //发送登录请求
-          let param = new URLSearchParams();
-          param.append("username", that.username);
-          param.append("password", that.password);
-          that.axios.post("/api/login", param).then(({ data }) => {
-            if (data.flag) {
-              that.username = "";
-              that.password = "";
-              that.$store.commit("login", data.data);
-              that.$store.commit("closeModel");
-              that.$toast({ type: "success", message: "登录成功" });
-            } else {
-              that.$toast({ type: "error", message: data.message });
-            }
-          });
+      // 发送登录请求
+      let param = new URLSearchParams();
+      param.append("username", that.username);
+      param.append("password", that.password);
+      that.axios.post("/api/blog/login", param).then(({ data }) => {
+        if (data.code == 200) {
+          that.username = "";
+          that.password = "";
+          that.$store.commit("login", data.data);
+          that.$store.commit("closeModel");
+          that.$toast({ type: "success", message: "登录成功" });
+        } else {
+          that.$toast({ type: "error", message: data.msg });
         }
       });
-      // 显示验证码
-      captcha.show();
-    },
-    qqLogin() {
-      //保留当前路径
-      this.$store.commit("saveLoginUrl", this.$route.path);
-      if (
-        navigator.userAgent.match(
-          /(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i
-        )
-      ) {
-        // eslint-disable-next-line no-undef
-        QC.Login.showPopup({
-          appId: this.config.QQ_APP_ID,
-          redirectURI: this.config.QQ_REDIRECT_URI
-        });
-      } else {
-        window.open(
-          "https://graph.qq.com/oauth2.0/show?which=Login&display=pc&client_id=" +
-            +this.config.QQ_APP_ID +
-            "&response_type=token&scope=all&redirect_uri=" +
-            this.config.QQ_REDIRECT_URI,
-          "_self"
-        );
-      }
     }
   }
 };
 </script>
-
-<style scoped>
-.social-login-title {
-  margin-top: 1.5rem;
-  color: #b5b5b5;
-  font-size: 0.75rem;
-  text-align: center;
-}
-.social-login-title::before {
-  content: "";
-  display: inline-block;
-  background-color: #d8d8d8;
-  width: 60px;
-  height: 1px;
-  margin: 0 12px;
-  vertical-align: middle;
-}
-.social-login-title::after {
-  content: "";
-  display: inline-block;
-  background-color: #d8d8d8;
-  width: 60px;
-  height: 1px;
-  margin: 0 12px;
-  vertical-align: middle;
-}
-.social-login-wrapper {
-  margin-top: 1rem;
-  font-size: 2rem;
-  text-align: center;
-}
-.social-login-wrapper a {
-  text-decoration: none;
-}
-</style>
--- src/components/model/RegisterModel.vue
+++ src/components/model/RegisterModel.vue
@@ -1,20 +1,29 @@
 <template>
-  <v-dialog v-model="registerFlag" :fullscreen="isMobile" max-width="460">
+  <v-dialog v-model="registerFlag" :fullscreen="isMobile" max-width="500">
     <v-card class="login-container" style="border-radius:4px">
       <v-icon class="float-right" @click="registerFlag = false">
         mdi-close
       </v-icon>
       <div class="login-wrapper">
-        <!-- 用户名 -->
+        <!-- 账号 -->
         <v-text-field
           v-model="username"
-          label="邮箱号"
-          placeholder="请输入您的邮箱号"
+          label="账号"
+          placeholder="请输入您的账号"
+          clearable
+          @keyup.enter="register"
+        />
+        <!-- 邮箱号 -->
+        <v-text-field
+          v-model="email"
+          class="mt-4"
+          label="邮箱"
+          placeholder="请输入您的邮箱"
           clearable
           @keyup.enter="register"
         />
         <!-- 验证码 -->
-        <div class="mt-7 send-wrapper">
+        <div class="mt-4 send-wrapper">
           <v-text-field
             maxlength="6"
             v-model="code"
@@ -29,7 +38,7 @@
         <!-- 密码 -->
         <v-text-field
           v-model="password"
-          class="mt-7"
+          class="mt-4"
           label="密码"
           placeholder="请输入您的密码"
           @keyup.enter="register"
@@ -39,7 +48,7 @@
         />
         <!-- 注册按钮 -->
         <v-btn
-          class="mt-7"
+          class="mt-4"
           block
           color="red"
           style="color:#fff"
@@ -48,7 +57,7 @@
           注册
         </v-btn>
         <!-- 登录 -->
-        <div class="mt-10 login-tip">
+        <div class="mt-7 login-tip">
           已有账号？<span @click="openLogin">登录</span>
         </div>
       </div>
@@ -63,6 +72,7 @@ export default {
       username: "",
       code: "",
       password: "",
+      email: "",
       flag: true,
       codeMsg: "发送",
       time: 60,
@@ -76,28 +86,19 @@ export default {
     },
     sendCode() {
       const that = this;
-      // eslint-disable-next-line no-undef
-      var captcha = new TencentCaptcha(this.config.TENCENT_CAPTCHA, function(
-        res
-      ) {
-        if (res.ret === 0) {
-          //发送邮件
-          that.countDown();
-          that.axios
-            .get("/api/users/code", {
-              params: { username: that.username }
-            })
-            .then(({ data }) => {
-              if (data.flag) {
-                that.$toast({ type: "success", message: "发送成功" });
-              } else {
-                that.$toast({ type: "error", message: data.message });
-              }
-            });
-        }
-      });
-      // 显示验证码
-      captcha.show();
+      // 发送邮件
+      that.countDown();
+      that.axios
+        .get("/api/blog/register/code", {
+          params: { email: that.email }
+        })
+        .then(({ data }) => {
+          if (data.code == 200) {
+            that.$toast({ type: "success", message: "发送成功" });
+          } else {
+            that.$toast({ type: "error", message: data.message });
+          }
+        });
     },
     countDown() {
       this.flag = true;
@@ -114,7 +115,7 @@ export default {
     },
     register() {
       var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-      if (!reg.test(this.username)) {
+      if (!reg.test(this.email)) {
         this.$toast({ type: "error", message: "邮箱格式不正确" });
         return false;
       }
@@ -129,14 +130,15 @@ export default {
       const user = {
         username: this.username,
         password: this.password,
+        email: this.email,
         code: this.code
       };
-      this.axios.post("/api/register", user).then(({ data }) => {
-        if (data.flag) {
+      this.axios.post("/api/blog/register", user).then(({ data }) => {
+        if (data.code == 200) {
           let param = new URLSearchParams();
           param.append("username", user.username);
           param.append("password", user.password);
-          this.axios.post("/api/login", param).then(({ data }) => {
+          this.axios.post("/api/blog/login", param).then(({ data }) => {
             this.username = "";
             this.password = "";
             this.$store.commit("login", data.data);
@@ -144,7 +146,7 @@ export default {
           });
           this.$toast({ type: "success", message: "登录成功" });
         } else {
-          this.$toast({ type: "error", message: data.message });
+          this.$toast({ type: "error", message: data.msg });
         }
       });
     }
@@ -167,7 +169,7 @@ export default {
     }
   },
   watch: {
-    username(value) {
+    email(value) {
       var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
       if (reg.test(value)) {
         this.flag = false;
